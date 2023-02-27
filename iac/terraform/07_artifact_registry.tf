4879f732bdee41fe5916681dcd3c5fbddf426d57
Codrin H <codrin.hanachiuc@gmail.com>
2023-02-27 14:42:02 +0200

    fix: registry name

--- iac/terraform/07_artifact_registry.tf
+++ iac/terraform/07_artifact_registry.tf
@@ -1,7 +1,7 @@
 resource "google_artifact_registry_repository" "docker" {
   for_each = toset([
     "docker-dummy-app",
-    "dockder-dummy-pdf-or-png",
+    "docker-dummy-pdf-or-png",
     "helm-dummy-app",
     "helm-dummy-pdf-or-png"
   ])
