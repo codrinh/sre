24c0018d334f62e8f03b5bf3fa128fa3aa44ee1e
Codrin H <codrin.hanachiuc@gmail.com>
2023-03-06 11:36:57 +0200

    fix: terraform fmt

--- iac/terraform/05_k8s_cluster.tf
+++ iac/terraform/05_k8s_cluster.tf
@@ -23,12 +23,12 @@ resource "google_container_cluster" "primary" {
 
   }
 
-    monitoring_config {
-      enable_components = ["SYSTEM_COMPONENTS"]
-      managed_prometheus {
-        enabled = true
-      }
+  monitoring_config {
+    enable_components = ["SYSTEM_COMPONENTS"]
+    managed_prometheus {
+      enabled = true
     }
+  }
   release_channel {
     channel = "REGULAR"
   }
